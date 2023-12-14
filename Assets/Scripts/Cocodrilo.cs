a841a704c9e65300f5b591cd01d278e19c80035a
YerayDS <yeraydsh@gmail.com>
2023-12-14 17:32:26 +0100

    Vision Especies

---
+++ Assets/Scripts/Cocodrilo.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cocodrilo : MonoBehaviour
+{
+    public float radio;
+    [Range(0, 360)]
+    public float angulo;
+    public GameObject playerRef;
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+    public bool puedeVer;
+    public GameObject cosa;
+
+    private void Start()
+    {
+        playerRef = this.gameObject;
+        StartCoroutine(FOVRoutine());
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        float delay = 0.2f;
+        WaitForSeconds wait = new WaitForSeconds(delay);
+        while (true)
+        {
+            yield return wait;
+            ComprobarVision();
+        }
+    }
+
+    private void ComprobarVision()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radio, targetMask);
+
+        if (rangeChecks.Length > 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            // Utilizar el producto punto para verificar el ángulo
+            float dotProduct = Vector3.Dot(transform.forward, directionToTarget);
+
+            // Establecer un umbral para el ángulo (ajustar según sea necesario)
+            float angleThreshold = Mathf.Cos(Mathf.Deg2Rad * (angulo / 2));
+            if (dotProduct > angleThreshold)
+            {
+                float distanciaToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanciaToTarget, obstructionMask))
+                {
+                    puedeVer = true;
+
+                }
+                else
+                {
+                    puedeVer = false;
+                }
+            }
+            else
+            {
+                puedeVer = false;
+            }
+        }
+        else if (puedeVer)
+        {
+            puedeVer = false;
+        }
+    }
+}
