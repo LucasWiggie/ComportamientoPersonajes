c96195242830d6722aa1c7f0684e7641d77ca932
Lucas Rubio <81293638+LucasWiggie@users.noreply.github.com>
2023-12-15 19:58:25 +0100

    Scripts BT Hambre Croc

---
+++ Assets/Scripts/BTScripts/CustomNodes/Croc_Comer.cs
@@ -0,0 +1,22 @@
+using MBT;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomNodes
+{
+    // Empty Menu attribute prevents Node to show up in "Add Component" menu.
+    [AddComponentMenu("")]
+    [MBTNode(name = "CustomNodes/Croc_Comer")]
+
+    public class Croc_Comer : Leaf
+    {
+
+        // This is called every tick as long as node is executed
+        public override NodeResult Execute()
+        {
+            // AQUI LA EJECUCIÓN DE QUE EL COCODRILO SE MUEVA A LA CAZA
+            return NodeResult.success;
+        }
+    }
+}
