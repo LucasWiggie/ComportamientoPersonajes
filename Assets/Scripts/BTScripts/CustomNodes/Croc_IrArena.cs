cc4a51bc4d1016b0c16e88281e75259bc028acc3
Lucas Rubio <81293638+LucasWiggie@users.noreply.github.com>
2023-12-15 22:47:28 +0100

    BTs cocodrilo planteados

---
+++ Assets/Scripts/BTScripts/CustomNodes/Croc_IrArena.cs
@@ -0,0 +1,22 @@
+using MBT;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomNodes
+{
+    // Empty Menu attribute prevents Node to show up in "Add Component" menu.
+    [AddComponentMenu("")]
+    [MBTNode(name = "CustomNodes/Croc_IrArena")]
+
+    public class Croc_IrArena : Leaf
+    {
+
+        // This is called every tick as long as node is executed
+        public override NodeResult Execute()
+        {
+            // AQUI LA EJECUCIÓN DE QUE EL COCODRILO SE MUEVA A LA ARENA
+            return NodeResult.success;
+        }
+    }
+}
